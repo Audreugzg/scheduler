d2a0c47844520a59870cb16dfb8aea11a121a40e
Zuge <audreygaozu@hotmail.com>
2022-07-13 16:01:39 -0700

    add the Show Component

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -2,11 +2,6 @@ import React from "react";
 import "components/InterviewerListItem.scss";
 import classNames from "classnames";
 
-// const props = {
-//   id: 1,
-//   name: "Sylvia Palmer",
-//   avatar: "https://i.imgur.com/LpaY82x.png"
-// };
 
 export default function InterviewerListItem(props) {
   let interviewerClass = classNames('interviewers__item',{'interviewers__item--selected' : props.selected});
