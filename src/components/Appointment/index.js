e5c8c27ba0c5a58a72dea723ba9bdc31e1357bcf
Zuge <audreygaozu@hotmail.com>
2022-07-13 18:02:33 -0700

    display appoitments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "./styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 
 export default function Appointment (props) {
@@ -7,6 +10,15 @@ export default function Appointment (props) {
 
   return (
     <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? (
+        <Show
+          name={props.interview.student}
+          interviewer={props.interview.interviewer.name}
+        />
+      ) : (
+        <Empty />
+      )}
   
     </article>
   );
